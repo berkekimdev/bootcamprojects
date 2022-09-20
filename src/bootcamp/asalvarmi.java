b20ffb17b634bfe06b7223e7524ad573c5e9da80
sudkostik <berkekim1@gmail.com>
2022-09-20 05:16:12 +0300

    bootcampa

---
+++ src/bootcamp/asalvarmi.java
@@ -0,0 +1,55 @@
+package bootcamp;
+
+public class asalvarmi {
+
+    public static void main(String[] args) {
+
+
+    int number = 2;
+    int remainder= number% 2;
+
+    boolean isPrime = true;
+
+    if(number==1) {
+
+        System.out.println("Sayı asal değildir");
+    }
+
+        if(number<1) {
+
+            System.out.println("Geçersiz Sayı");
+        }
+
+        for(int i=2; i<number;i++) {
+            if(number %i == 0) {
+                isPrime=false;
+            }
+        }
+    if(isPrime) {
+        System.out.println("Sayı asaldır");
+    }
+    else {
+        System.out.println("Sayı asal değildir");
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
---
+++ src/bootcamp/fordonguler.java
@@ -0,0 +1,41 @@
+package bootcamp;
+
+import java.sql.SQLOutput;
+
+public class fordonguler {
+
+    public static void main(String[] args) {
+
+        for(int i =2; i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+
+
+        int i =1;
+        while(i<10) {
+
+            System.out.println(i);
+            i+=2;
+        }
+
+        System.out.println("While Döngüsü Bitti");
+
+        int j=100;
+        do{
+            System.out.println("Loglandı");
+            System.out.println(j);
+            j+=2;
+        }while(j<10);
+        System.out.println("Do-While Döngüsü Bitti");
+
+
+
+
+
+
+
+
+
+    }
+}
